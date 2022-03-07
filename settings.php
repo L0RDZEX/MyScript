f16eb85602c422402d772bd7791545870cf4f84e
L0RDZEX <naufalakbaremail012@gmail.com>
2022-03-07 10:34:51 +0000

    first

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+
+    // THIS FILE IS MADE TO MAKE CUSTOMIZING MUCH EASIER. SIMPLY ENTER YOUR SERVER DETAILS HERE AND THEY WILL CHANGE ITSELF.
+	define('SVRNAME', 'Merpati Roleplay');// the name of your server
+	define('OWNER', 'All Team Administrator Merpati'); //server owner name
+	define('SVRIP', 'samp.merpatiroleplay.my.id'); //Your server's IP.
+	define('SVRSLOGAN', 'Merpati Roleplay #VOICEONLY.'); //Your server's slogan
+
+	require("samp_query.php");
+	$serverIP = "samp.merpatiroleplay.my.id"; // Add in your server IP. DO NOT ADD THE PORT HERE, ADD IT BELOW!
+	$serverPort = 7771; // Server Port.
+?>
